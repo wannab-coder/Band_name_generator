540bbf574b0b4cfe2f094bb989d3e343d7a73af8
wannab-coder <110335736+wannab-coder@users.noreply.github.com>
2022-08-18 20:00:41 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Welcome to the Band Name Generator.")
+
+myname=input("What's name of the city you grew up in?\n")
+
+petname=input("What's your pet's name?\n")
+
+bandname=myname+" "+petname
+
+print("Your band name could be:"+" "+bandname)
